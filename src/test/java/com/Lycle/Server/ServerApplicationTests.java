1df9e888afd6defa3b9f5de41bc1ed2b89c6b84e
juu924 <osj8528@gmail.com>
2022-06-22 23:39:17 +0900

    [chore] inital commit

---
+++ src/test/java/com/Lycle/Server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lycle.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
