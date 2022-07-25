9afbbbd42ad8d400b22f7376d19656ee5dc12760
juu924 <osj8528@gmail.com>
2022-07-25 15:43:49 +0900

    [chore] 로그인/회원가입 시 validation annotation 활용하여 간단한 유효성 검사(#LYC 19~20)

--- src/main/java/com/Lycle/Server/dto/User/UserJoinDto.java
+++ src/main/java/com/Lycle/Server/dto/User/UserJoinDto.java
@@ -4,10 +4,15 @@ import com.Lycle.Server.domain.User;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 public class UserJoinDto {
+    @NotBlank(message = "이메일 주소를 입력해주세요.")
     private String email;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+    @NotBlank(message = "닉네임을 입력해주세요")
     private String nickname;
 
     public User toEntity() {
--- src/main/java/com/Lycle/Server/dto/User/UserLoginDto.java
+++ src/main/java/com/Lycle/Server/dto/User/UserLoginDto.java
@@ -1,10 +1,13 @@
 package com.Lycle.Server.dto.User;
 
 import lombok.Getter;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 public class UserLoginDto {
+   @NotBlank(message = "이메일 주소를 입력해주세요.")
    private String email;
+   @NotBlank(message = "비밀번호를 입력해주세요.")
    private String password;
 
    public UserLoginDto(String email, String password){
