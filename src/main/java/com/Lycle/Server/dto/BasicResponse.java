a3ef8b163e133d2af0c5420643bdeb504b69e21f
juu924 <osj8528@gmail.com>
2022-07-25 17:47:18 +0900

    [add] Response Entity 추가 (#LYC-31)

---
+++ src/main/java/com/Lycle/Server/dto/BasicResponse.java
@@ -0,0 +1,20 @@
+package com.Lycle.Server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import java.util.List;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasicResponse {
+    private int code;
+    private HttpStatus httpStatus;
+    private String message;
+    private int count;
+    private List<Object> result;
+}
