a20f59a63639dfa167dfd2947bb57d45decff3f3
juu924 <osj8528@gmail.com>
2022-08-01 16:03:46 +0900

    [feat] 사용자 프로필 조회 기능 (#LYC-21)

--- src/main/java/com/Lycle/Server/service/UserService.java
+++ src/main/java/com/Lycle/Server/service/UserService.java
@@ -1,6 +1,7 @@
 package com.Lycle.Server.service;
 
 import com.Lycle.Server.domain.jpa.UserRepository;
+import com.Lycle.Server.dto.User.SearchProfileWrapper;
 import com.Lycle.Server.dto.User.SearchUserWrapper;
 import com.Lycle.Server.dto.User.UserJoinDto;
 import lombok.RequiredArgsConstructor;
@@ -11,29 +12,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-  private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-  @Transactional
-  public Long saveUser(UserJoinDto userJoinDto){
-     return userRepository.save(userJoinDto.toEntity()).getId();
-  }
+    @Transactional
+    public Long saveUser(UserJoinDto userJoinDto) {
+        return userRepository.save(userJoinDto.toEntity()).getId();
+    }
 
-  @Transactional(readOnly = true)
-   public SearchUserWrapper searchUser(String email, String password){
-      return userRepository.findByEmailAndPassword(email,password)
-              .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 id 입니다."));
-  }
+    @Transactional(readOnly = true)
+    public SearchUserWrapper searchUser(String email, String password) {
+        return userRepository.findByEmailAndPassword(email, password)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 id 입니다."));
+    }
 
-  @Transactional(readOnly = true)
-    public boolean verifyEmail(String email){
-      return userRepository.existsByEmail(email);
-  }
+    @Transactional(readOnly = true)
+    public boolean verifyEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 
-  @Transactional(readOnly = true)
-    public boolean verifyNickname(String nickname){
-      return userRepository.existsByNickname(nickname);
-  }
+    @Transactional(readOnly = true)
+    public boolean verifyNickname(String nickname) {
+        return userRepository.existsByNickname(nickname);
+    }
 
-
+    @Transactional(readOnly = true)
+    public SearchProfileWrapper searchProfile(Long id) {
+        return userRepository.findUserById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원 입니다."));
+    }
 
 }
